4471e55c096d19c9672841b1c26168019c40bad8
coolsun <hui.liang@nokia-sbell.com>
2017-12-29 20:29:43 +0800

    update MT related resource files, and upload MT zip file for work episode 1

--- test/testEvent.js
+++ test/testEvent.js
@@ -1,288 +1,197 @@
+/*
+ unit test cases for Event related functionalities
+*/
+
 'use strict';
 var supertest = require('supertest');
+var eventUtil = require('../lib/eventUtil.js');
 var assert = require('assert');
-var Promise = require('bluebird');
 
-const {
-  auth,
-  init_user, 
-  createUser, 
-  createGroup,
-  createEvent,
-  createCommingEvent,
-  createPassedEvent} = require('./utils/common')
+var init_user = function(user_name, id) {
+  var user = {};
+  user.fullname = user_name;
+  user.email = 'gang-layner.wang@nokia.com';
+  user.id = id;
+  return user;
+};
 
-const BASE_URL = '/'
-const URLS = {
-  EVENT_DO_CREATE: groupId =>`${BASE_URL}group/${groupId}/newEvent`,
-  EVENT_DO_EDIT: (groupid, eventid)=>`${BASE_URL}group/${groupid}/event/${eventid}/eventDoEdit`,
-  EVENT_INVITE_FRIEND: eventid=>`${BASE_URL}event/${eventid}/InviteFriendAjax`,
-  EVENT_ADD_MEMBER: eventid=>`${BASE_URL}event/${eventid}/addEventMemberAjax`, //addUsersToEvent
-  EVENT_PUBLISH: (groupid, eventid) => `${BASE_URL}group/{groupid}/event/${eventid}/publish`,
-  EVENT_SHOW_AJAX: (groupId, eventId) =>`${BASE_URL}group/${groupId}/event/${eventId}/show`,
-  EVENT_SHOW_MY_EVENT: BASE_URL + 'user/showMyEventAjax',
-  GROUP_LOAD_ONE: BASE_URL + 'group/1/loadoneGroupAjax',
-  GROUP_UPDATE: BASE_URL + 'group/updateAjax',
-  GROUP_DELETE: BASE_URL + 'group/deleteAjax',
-}
-const userName = 'yaolin'
-const userId = '61467748'
-const groupName= 'group1'
-const eventName= 'event1'
-const user = init_user(userName, userId)
+describe('event and eventUtil', function() {
 
-function createEventAjax(agent, groupid, topic, publish=false){
-  return new Promise((resolve, reject)=>{
-    agent
-    .post(URLS.EVENT_DO_CREATE(groupid))
-    .send({
-      Topic: topic,
-      Group: groupid,
-      Address: '3',
-      beginDate: '2017-11-14',
-      BeginTime: '23:30',
-      endDate: '2017-11-16',
-      EndTime: '10:30',
-      EventDetail: 'detail',
-      Tag:[],
-      Publish: publish,
-    })
-    .expect(200)
-    .end((err, res)=>{
-      err? reject(err): resolve(res)
-    })
-  })
-}
+  describe('event relevant functions', function() {
 
-function editEventAjax(agent, groupid, eventid, topic, publish=false){
-  return new Promise((resolve, reject)=>{
-    agent
-    .post(URLS.EVENT_DO_EDIT(groupid, eventid))
-    .send({
-      Topic: topic,
-      Group: groupid,
-      Address: '3',
-      beginDate: '2017-11-14',
-      BeginTime: '23:30',
-      endDate: '2017-11-16',
-      EndTime: '10:30',
-      EventDetail: 'detail',
-      Tag:[],
-      Publish: publish,
-    })
-    .expect(200)
-    .end((err, res)=>{
-      err? reject(err): resolve(res)
-    })
-  })
-}
+    var eventID;
+    var eventOwner;
 
-function inviteFriendAjax(agent, eventid, from, email){
-  return new Promise((resolve, reject)=>{
-    agent
-    .post(URLS.EVENT_INVITE_FRIEND(eventid))
-    .send({
-      from: from,
-      email: email,
-      eid: eventid, //event id is mandantory for privilege check???
-    })
-    .expect(200)
-    .end((err, res)=>{
-      err? reject(err): resolve(res)
-    })
-  })
-}
-
-function eventAddMemberAjax(agent, eventid, users){
-  return new Promise((resolve, reject)=>{
-    agent
-    .post(URLS.EVENT_ADD_MEMBER(eventid))
-    .send({
-      users: users,
-      eid: eventid,
-    })
-    .expect(200)
-    .end((err, res)=>{
-      err? reject(err): resolve(res)
-    })
-  })
-}
-
-function eventPublish( agent, groupid, eventid ){
-  return new Promise((resolve, reject)=>{
-    agent
-    .post(URLS.EVENT_PUBLISH(groupid, eventid))
-    .send({
-      gid: groupid,
-      eid: eventid,
-    })
-    .expect(302)
-    .end((err, res)=>{
-      err? reject(err): resolve(res)
-    })
-  })
-}
-
-function eventShowMyEvent( agent ){
-  return new Promise((resolve, reject)=>{
-    agent
-    .post(URLS.EVENT_SHOW_MY_EVENT)
-    .send()
-    .expect(200)
-    .end((err, res)=>{
-      err? reject(err): resolve(res)
-    })
-  })
-}
-
-describe('Event', function() {
-
-  var agent;
-  var createdGroup
-  var createdUser
-  var createdEvent
-
-  before(function(done) {
-    agent = supertest.agent(sails.hooks.http.app)
-
-    Promise.resolve()
-    .then(User.destroy)
-    .then(Group.destroy)
-    .then(()=>{
-       return Promise.using(User.create(user), createdUser=>{
-         return Promise.using(createGroup({name: groupName, userIds: [createdUser._id, '61467749']}, createdUser), createGroup=>{
-           return auth(agent, createdUser)
-         })
-      })
-    })
-    .then(()=>{
-      done()
-    })
-  });
-
-	describe('EventController.doCreate', function(){
-
-
-    function createEventInModal(groupId){
-      const params = {
-        Topic: 'topic',
-        group: groupId,
-        Address: '3',
-        beginDate: '2017-11-14',
-        BeginTime: '23:30',
-        endDate: '2017-11-16',
-        EndTime: '10:30',
-        EventDetail: 'detail',
-        Tag:[],
-        Publish: false
-      }
-      return Event.create(params)
-    }
-
-		it('doCreate should be ok', done => {
-      Promise.using(Group.findOne({name: groupName}), findGroup => {
-        createEventAjax(agent, findGroup.id, 'topic2')
-        .then(res=>{
-          assert.equal(res.body.id, findGroup.id);
+    before(function(done) {
+      User.destroy()
+        .then(function(result) {
+          return Group.destroy();
+        })
+        .then(function(result) {
+          return Event.destroy();
+        })
+        .then(function(result) {
+          var user = init_user('WangGang', '10239936');
+          return User.create(user);
+        })
+        .then(function(createdUser) {
+          var groupObj = {};
+          groupObj.name = 'test group';
+          groupObj.desc = 'hello world';
+          groupObj.owner = createdUser;
+          eventOwner = createdUser;
+          return Group.create(groupObj);
+        })
+        .then(function(createdGroup) {
+          var eventObj = {};
+          eventObj.owner = eventOwner;
+          eventObj.desc = 'for test';
+          eventObj.group = createdGroup;
+          return Event.create(eventObj);
+        })
+        .then(function(createdEvent) {
+          eventID = createdEvent.id;
           done();
         })
-        .catch(done)
-		});
 
-    it('event showAjax should be ok', done=>{
-      Promise.using(Group.findOne({name: groupName}), findGroup => {
-        createEventInModal(findGroup.id)
-        .then((createdEvent)=>{
-          agent
-            .get(URLS.EVENT_SHOW_AJAX(findGroup.id, createdEvent.id))
-            .send()
-            .expect(200)
-            .end(function(err, res) {
-              //assert.equal(res.body.id, findGroup.id);
-              if (err) {
-                return done(err);
+    });
+
+    it('add one user to event, test interface', function(done) {
+
+      var user = init_user('Xi JinPing', '00000001');
+      User.create(user)
+        .then(function(createdUser) {
+          eventUtil.addOneUserToEvent(user, eventID, function(err, updatedEvent) {
+            if(err) {
+              done(err);
+            } else {
+              assert.equal(updatedEvent.user.length, 1);
+              for(var i = 0; i < updatedEvent.user.length; i++) {
+                if(updatedEvent.user[i].id === user.id) {
+                  done();
+                  break;
+                }
               }
-              done();
-            });
+              if(i === updatedEvent.user.length) {
+                throw('add user to event failed');
+              }
+            }
           })
         })
-      })
-    })
-  })
+        .catch(function(err) {
+          done(err);
+        });
 
+    });
 
-	describe('EventController.doEdit', function(){
-		it('doEdit should be ok', function(done) {
-      Promise.using(Group.findOne({name: groupName}), findGroup=>{
-        createEventAjax(agent, findGroup.id, 'topic2')
-        .then(result=>{
-          const eventid = result.body.id
-          return editEventAjax(agent, findGroup.id, eventid, 'topic3')
-        })
-        .then(()=>{
-          done()
-        })
-      })
-		});
-  })
+  });
 
-	describe('EventController.invitefriends', function(){
-		it('invitefriends should be ok', function(done) {
-      Promise.using(Group.findOne({name: groupName}), findGroup=>{
-        createEventAjax(agent, findGroup.id, 'topic2')
-        .then(result=>{
-          const eventid = result.body.id
-          return inviteFriendAjax(agent, eventid, userName, 'yao.lin@nokia-sbell.com' )
-        })
-        .then(()=>{
-          done()
-        })
-      })
-		});
-  })
+  describe('add one user to group', function() {
 
-	describe('EventController.addEventMemberAjax', function(){
-		it('addEventMemberAjax should be ok', function(done) {
-      Promise.using(Group.findOne({name: groupName}), findGroup=>{
-        createEventAjax(agent, findGroup.id, 'topic2')
-        .then(result=>{
-          const eventid = result.body.id
-          return eventAddMemberAjax(agent, eventid, [userId])
-        })
-        .then(()=>{
-          done()
-        })
-      })
-		});
-  })
+    var createdGroupId;
 
-	describe('EventController.publish', function(){
-		it('publish should be ok', function(done) {
-      Promise.using(Group.findOne({name: groupName}), findGroup=>{
-        createEventAjax(agent, findGroup.id, 'topic2')
-        .then(result=>{
-          const eventid = result.body.id
-          return eventPublish(agent, findGroup.id, eventid)
-        })
-        .then(()=>{
-          done()
-        })
-      })
-		});
-  })
+    before(function(done) {
 
-	describe('EventController.showMyEventAjax', function(){
-		it('showMyEventAjax should be ok', function(done) {
-      Promise.using(Group.findOne({name: groupName}), findGroup=>{
-        createEventAjax(agent, findGroup.id, 'topic2')
-        .then(result=>{
-          const eventid = result.body.id
-          return eventShowMyEvent(agent)
+      User.destroy()
+        .then(function(result) {
+          return Group.destroy();
         })
-        .then(()=>{
-          done()
+        .then(function(result) {
+          var user = init_user('WangGang', '10239936');
+          return User.create(user);
         })
-      })
-		});
-  })
+        .then(function(createdUser) {
+          var groupObj = {};
+          groupObj.name = 'test group';
+          groupObj.desc = 'hello world';
+          groupObj.owner = createdUser;
+          return Group.create(groupObj);
+        })
+        .then(function(createdGroup) {
+          createdGroupId = createdGroup.id;
+          done();
+        });
+    });
+
+    it('add one user to group', function(done) {
+
+      var user = init_user('Xi JinPing', '00000001');
+      User.create(user)
+        .then(function(createdUser) {
+          eventUtil.addUserToGroup(createdGroupId, user, function(err, newGroup) {
+            if(err) { return done(err); }
+            assert.equal(newGroup.user[0].id, user.id);
+            done();
+          })
+        });
+    });
+
+  });
+
+  describe('get mail list of event users and send notify mail', function() {
+    var eventID;
+
+    beforeEach(function(done) {
+
+      var user1 = init_user('wanggang', '10239936');
+      var user2 = init_user('xi jinping', '00000001');
+      var createdEvent;
+      var createdUser1;
+      var createdUser2;
+
+      Event.destroy()
+        .then(function(results) {
+          return Mail.destroy();
+        })
+        .then(function(results) {
+          return User.destroy();
+        })
+        .then(function(results) {
+          var event_obj = {};
+          event_obj.topic = 'NOKIA 50th birthday Celebration';
+          event_obj.desc = 'Hello NOKIA';
+          event_obj.address = '19th floor';
+          var date = new Date();
+          event_obj.begindate = date;
+          date = new Date(date.getTime() + 1000);
+          event_obj.enddate = date;
+          return Event.create(event_obj);
+        })
+        .then(function(created) {
+          createdEvent = created;
+          eventID = created.id;
+          return User.create(user1)
+        })
+        .then(function(createdUser) {
+          createdUser1 = createdUser;
+          return User.create(user2);
+        })
+        .then(function(createdUser) {
+          createdUser2 = createdUser;
+          createdEvent.user.add(createdUser1);
+          return createdEvent.save();
+        })
+        .then(function() {
+          createdEvent.user.add(createdUser2);
+          return createdEvent.save();
+        })
+        .then(function(createdUser2) {
+          done();
+        })
+        .catch(done);
+    });
+
+    it('get the mail list of one event', function(done) {
+
+      eventUtil.getEventUsersMail(eventID, function(err, mail_addrs) {
+        if(err) {
+          done(err);
+        } else {
+          assert.equal(mail_addrs[0], 'gang-layner.wang@nokia.com');
+          assert.equal(mail_addrs[1], 'gang-layner.wang@nokia.com');
+          done();
+        }
+      });
+    });
+  });
 })
--- test/testGroup.js
+++ test/testGroup.js
@@ -1,202 +1,149 @@
-'use strict';
+/*
+ unit test cases for Group related functionalities
+*/
 
+'use strict';
 var supertest = require('supertest');
 var groupUtil = require('../lib/groupUtil');
 var assert = require('assert');
 
-const { auth, init_user, createUser, createGroup, createEvent, createCommingEvent, createPassedEvent} = require('./utils/common')
-
-const BASE_URL = '/'
-const URLS = {
-  GROUP_GET: BASE_URL + 'group/getGroupsAjax',
-  GROUP_CREATE: BASE_URL + 'group/createAjax',
-  GROUP_LOAD_ONE: BASE_URL + 'group/1/loadoneGroupAjax',
-  GROUP_UPDATE: BASE_URL + 'group/updateAjax',
-  GROUP_DELETE: BASE_URL + 'group/deleteAjax',
-  SHOW_MY_GROUP: BASE_URL + 'user/showMyGroupAjax',
-  SHOW_MY_PROFILE: BASE_URL + 'user/showProfileAjax',
-}
-const userName = 'yaolin'
-const userId = '61467748'
-const groupName= 'group1'
-const eventName= 'event1'
-const user = init_user(userName, userId)
+var init_user = function(user_name, userid){
+  var user = {};
+  user.fullname = user_name;
+  user.email = 'gang-layner.wang@nokia.com';
+  user.id = userid;
+  return user;
+};
 
 describe('Group', function() {
 
-  var agent;
+  describe('Group creation', function(){
+    it('should be created successfully and redirect to new page', function(done) {
 
-  before(function(done) {
-    agent = supertest.agent(sails.hooks.http.app)
+      supertest(sails.hooks.http.app)
+        .post('/newGroup')
+        .send({
+          Name: 'groupTest',
+          Desc: 'This is one group for test',
+          Pic: 123456
+        })
+        .expect(200)
+        .end(function(err, res) {
+          if (err) {
+            console.log(res.text);
+            return done(err);
+          }
+          done();
+        });
+    });
 
-    Promise.resolve()
-    .then(User.destroy)
-    .then(Group.destroy)
-    .then(User.create.bind(null, user))
-    .then(auth.bind(null, agent, user))
-    .then(done)
+    it('should be created failed and response error message', function(done) {
+
+      supertest(sails.hooks.http.app)
+        .post('/newGroup')
+        .send({
+          Name: '',
+          Desc: 'This is one group for test'
+        })
+        .expect(500)
+        .expect('{\n  "error": "group name or group description is null"\n}')
+        .end(function(err, res) {
+          if (err) {
+            console.log(res.text);
+            return done(err);
+          }
+          done();
+        });
+    });
+
   });
 
+  describe('Get the group user mail list', function(){
 
-	describe('GroupController.createAjax', function(){
-    after(done=>{
+    var groupid;
+
+    beforeEach(function(done){
+      
+      var user1 = init_user('wanggang', '10239936');
+      var user2 = init_user('xi jinping', '00000001');
+      var createdGroup;
+      var createdUser1;
+      var createdUser2;
+
       Group.destroy()
-      .then(()=>done())
-    })
-
-		it('should be created successfully and redirect to new page', function(done) {
-      agent
-				.post(URLS.GROUP_CREATE)
-				.send({
-					Name: 'groupTest',
-					Desc: 'This is one group for test',
-					Pic: 123456
-				})
-				.expect(200)
-				.end(function(err, res) {
-					if (err) {
-						//console.log(res.text);
-						return done(err);
-					}
-					done();
-				});
-		});
-
-		it('should be created failed and response error message', function(done) {
-
-			agent
-				.post(URLS.GROUP_CREATE)
-				.send({
-					Name: 'groupTest',
-					Desc: 'This is one group for test'
-				})
-				.expect(400)
-				.end(function(err, res) {
-					done();
-				});
-		});
-	});
-
-	describe('GroupController.getGroupsAjax', function(){
-		it('should be correct get groups info', function(done) {
-
-      agent
-				.post(URLS.GROUP_GET)
-				.send()
-				.expect(200)
-				.end(function(err, res) {
-					if (err) {
-						return done(err);
-					}
-					done();
-				});
-		});
-	});
-
-
-	describe('loadoneGroupAjax', function(){
-
-    before(done=>{
-      Promise.resolve()
-      .then(_=> createUser('yaolin', '61467748'))
-      .then(_=> createUser('yaolin2', '61467749'))
-      .then(_=> User.findOne({uid: userName}))
-      .then(user => createGroup({name: groupName}, user))
-      .then(group=>{
-        return Promise.all([createPassedEvent(group), createCommingEvent(group)])
+      .then(function(results){
+        return User.destroy();
       })
-      .then(()=>done())
-      .catch(done)
-    })
-
-    it('loadoneGroupAjax should be correct', (done)=>{
-
-      agent
-				.get(URLS.GROUP_LOAD_ONE)
-				.expect(200)
-				.end(function(err, res) {
-          //console.log(res.body)
-          const {comingEvents, RecentEvent, pastEvents, events, group, user, ingroup} = res.body
-          assert.equal(comingEvents.length, 1)
-          assert.equal(pastEvents.length, 1)
-          assert.equal(events.length, 2)
-          assert.equal(group.name, groupName)
-          assert.equal(user.uid, userName)
-          assert.equal(ingroup, true)
-          assert.equal(group.user.length, 3)
-          assert.equal(group.user[0].fullname, userName)
-
-					if (err) {
-						return done(err);
-					}
-					done();
-				});
-    })
-  })
-
-	describe('UserController.showMyGroupAjax', function(){
-    before(done=>{
-      Promise.resolve()
-      .then(_=> createUser('yaolin', '61467748'))
-      .then(_=> createUser('yaolin2', '61467749'))
-      .then(_=> User.findOne({uid: userName}))
-      .then(user => createGroup({name: groupName, userIds: [user._id]}, user))
-      .then(group=>{
-        return Promise.all([createPassedEvent(group), createCommingEvent(group)])
+      .then(function(results){
+        var group_obj = {};
+        group_obj.name ='test group';
+        group_obj.desc='hello world';
+        group_obj.owner=1;
+        group_obj.groupfd='/1.jpg';
+        group_obj.tags='innovation';
+        return Group.create(group_obj);    
       })
-      .then(()=>done())
-      .catch(done)
-    })
-
-    it('loadoneGroupAjax should be correct', (done)=>{
-
-      agent
-				.get(URLS.SHOW_MY_GROUP)
-				.expect(200)
-				.end(function(err, res) {
-          //console.log(res.body)
-          const {groups} = res.body
-
-          assert.equal(groups.length, 1);
-
-					if (err) {
-						done(err);
-					}
-					done();
-				});
-    })
-  })
-
-	describe('UserController.profile', function(){
-    before(done=>{
-      Promise.resolve()
-      .then(_=> createUser('yaolin', '61467748'))
-      .then(_=> createUser('yaolin2', '61467749'))
-      .then(_=> User.findOne({uid: userName}))
-      .then(user => createGroup({name: groupName, userIds: [user._id]}, user))
-      .then(group=>{
-        return Promise.all([createPassedEvent(group), createCommingEvent(group)])
+      .then(function(created){
+        createdGroup = created;
+        groupid = created.id;
+        return User.create(user1)
       })
-      .then(()=>done())
-      .catch(done)
-    })
+      .then(function(createdUser){
+        createdUser1 = createdUser;
+        return User.create(user2);
+      })
+      .then(function(createdUser){
+        createdUser2 = createdUser;
+        createdGroup.user.add(createdUser1);
+        return createdGroup.save();
+      })
+      .then(function(){
+        createdGroup.user.add(createdUser2);
+        return createdGroup.save();
+      })
+      .then(function(createdUser2){
+        done();
+      })
+      .catch(done);
+    });
 
-    xit('show profile should be correct', (done)=>{
+    it('get the mail list of one group', function(done){
+      
+      groupUtil.getGroupUsersMail(groupid, function(err, mail_addrs){
+        if(err){
+          done(err);
+        }else{
+          assert.equal(mail_addrs.length, 2);
+          assert.equal(mail_addrs[0], 'gang-layner.wang@nokia.com');
+          assert.equal(mail_addrs[1], 'gang-layner.wang@nokia.com');
+          done();
+        }
+        
+      });
+    });
 
-      agent
-				.get(URLS.SHOW_MY_PROFILE)
-				.expect(200)
-				.end(function(err, res) {
-          //console.log(res.body)
+    it('send notify mail to group users', function(done){
 
-					if (err) {
-						done(err);
-					} else{
-            done();
-          }
-				});
-    })
-  })
-
-
+        MailService.startMailService();
+        Mail.destroy()
+        .then(function(saved){
+            var mail_obj ={};
+            mail_obj.subject ='hello world for group users';
+            mail_obj.content ='from layner';
+            mail_obj.date=new Date();
+            console.log(groupid);
+            groupUtil.sendNotifyMailToGroupUsers(groupid, mail_obj, function(err){
+              setTimeout(function(){  
+                Mail.find({state:'done'})
+                .then(function(result){
+                    assert.equal(result.length, 1);
+                    done();
+                })
+                .catch(done);
+              }, 30000);
+            });
+        })
+        .catch(done);
+    });
+  });
 });
